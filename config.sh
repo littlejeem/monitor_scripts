672f4a6369151fc90fa8edefb95f473a241f4bbd
James Livingstone <jlivin25@live.co.uk>
2019-10-07 10:35:53 +0100

    added config file

---
+++ config.sh
@@ -0,0 +1,13 @@
+####################
+### IP ADDRESSES ###
+####################
+EXPECTED_IP=INSERT_VPN_IP_HERE
+#
+#
+########################
+### PUSHOVER DETAILS ###
+########################
+APP_TOKEN=INSERT_TOKEN_HERE
+USER_KEY=INSERT_KEY_HERE
+#
+#
