3b1fabefb35c581d8555fbdb80b4979c0900a686
James Livingstone <jlivin25@live.co.uk>
2019-10-08 14:34:21 +0100

    added log functionality

--- install_monitor_scripts.sh
+++ install_monitor_scripts.sh
@@ -7,12 +7,13 @@
 DIR1=${PWD}
 source "$DIR1"/config.sh
 croncmd="$DIR1"/IPChecker.sh
-cronjob="* * * * * $croncmd"
+cronjob="* * * * * $croncmd >> $log_location"
 #
 #
 ##################
 ### RUN SCRIPT ###
 ##################
+mkdir -p $log_location
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 #
 #
