03c09d1520a7926bfec4d8b297e31a8e9cb9eaaf
James Livingstone <jlivin25@live.co.uk>
2019-10-08 14:33:47 +0100

    added log functionality

--- config.sh
+++ config.sh
@@ -1,19 +1,21 @@
-############################
-### SCRIPT FILE LOCATION ###
-############################
-DIR2="/home/USER/bin/" #<-----------alter USER to match the script user
+#############################
+### SCRIPT FILE LOCATIONS ###
+#############################
+SCRIPT_USER=""                              # enter intended script USER
+DIR2="/home/SCRIPT_USER/bin/"               # installation parent directory of repository
+log_location="/home/SCRIPT_USER/bin/logs/"  # log file location
 #
 #
 ####################
 ### IP ADDRESSES ###
 ####################
-EXPECTED_IP=INSERT_VPN_IP_HERE
+EXPECTED_IP=INSERT_VPN_IP_HERE              # the IP you expect your VPN to report
 #
 #
 ########################
 ### PUSHOVER DETAILS ###
 ########################
-APP_TOKEN=INSERT_TOKEN_HERE
-USER_KEY=INSERT_KEY_HERE
+APP_TOKEN=INSERT_TOKEN_HERE                 # application token from Pushover, you will need to create one first to genereate the "token"
+USER_KEY=INSERT_KEY_HERE                    # user api key, this is generated automatically in pushover when you register and is on the front page
 #
 #
