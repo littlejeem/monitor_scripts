15f0fce75dff977fa50bbd29d37006355d9ce5c8
James Livingstone <jlivin25@live.co.uk>
2019-10-08 17:54:01 +0100

    correct missing variables

--- config.sh
+++ config.sh
@@ -1,21 +1,21 @@
 #############################
 ### SCRIPT FILE LOCATIONS ###
 #############################
-SCRIPT_USER=""                              # enter intended script USER
-DIR2="/home/SCRIPT_USER/bin/"               # installation parent directory of repository
-log_location="/home/SCRIPT_USER/bin/logs/"  # log file location
+SCRIPT_USER=""                                # enter intended script USER
+DIR2="/home/$SCRIPT_USER/bin/"                # installation parent directory of repository
+log_location="/home/$SCRIPT_USER/bin/logs/"   # log file location
 #
 #
 ####################
 ### IP ADDRESSES ###
 ####################
-EXPECTED_IP=INSERT_VPN_IP_HERE              # the IP you expect your VPN to report
+EXPECTED_IP=INSERT_VPN_IP_HERE                # the IP you expect your VPN to report
 #
 #
 ########################
 ### PUSHOVER DETAILS ###
 ########################
-APP_TOKEN=INSERT_TOKEN_HERE                 # application token from Pushover, you will need to create one first to genereate the "token"
-USER_KEY=INSERT_KEY_HERE                    # user api key, this is generated automatically in pushover when you register and is on the front page
+APP_TOKEN=INSERT_TOKEN_HERE                   # application token from Pushover, you will need to create one first to genereate the "token"
+USER_KEY=INSERT_KEY_HERE                      # user api key, this is generated automatically in pushover when you register and is on the front page
 #
 #
