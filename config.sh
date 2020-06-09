c0d07a14576e8bce7fb50cd3602e289662fe4660
James Livingstone <jlivin25@live.co.uk>
2020-06-09 09:38:02 +0100

    begun to adjust variable naming for accepted conventions

--- config.sh
+++ config.sh
@@ -15,7 +15,7 @@ EXPECTED_IP=INSERT_VPN_IP_HERE                # the IP you expect your VPN to re
 ########################
 ### PUSHOVER DETAILS ###
 ########################
-APP_TOKEN=INSERT_TOKEN_HERE                   # application token from Pushover, you will need to create one first to genereate the "token"
-USER_KEY=INSERT_KEY_HERE                      # user api key, this is generated automatically in pushover when you register and is on the front page
+app_token=INSERT_TOKEN_HERE                   # application token from Pushover, you will need to create one first to genereate the "token"
+user_token=INSERT_KEY_HERE                      # user api key, this is generated automatically in pushover when you register and is on the front page
 #
 #
