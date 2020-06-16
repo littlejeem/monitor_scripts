1f5b41d889369f3c02eaca8c9d78398e147dd08d
James Livingstone <jlivin25@live.co.uk>
2020-06-16 10:53:59 +0100

    rework of script, needs testing

--- IPChecker.sh
+++ IPChecker.sh
@@ -7,34 +7,63 @@
 ############################################################################
 #
 #
-##############################
-### SET & IMPORT VARIABLES ###
-##############################
-TESTED_IP=$(curl ipinfo.io/ip)
-FILE=/tmp/IPChecker.lock
-source "$DIR1"/config.sh
-#
-#
-###############
-### TEST IP ###
-###############
-if [ "$EXPECTED_IP" == "$TESTED_IP" ]
- then echo "UP"
- else
- echo "DOWN"
- #
- #
-  if test -f "$FILE"
-  then echo "VPN Down but $FILE exists, a reset has not been completed"
-     exit 0
-  else
-  echo "VPN Down, stopping transmission-daemon"
-  systemctl stop transmission-daemon
+#+---------------------+
+#+---"Set Functions"---+
+#+---------------------+
+function pushover () {
   curl -s \
-    --form-string "token=$app_token" \
-    --form-string "user=$user_token" \
-    --form-string "message=VPN Down, transmission-daemon stopped" \
+    --form-string token="$app_token" \
+    --form-string user="$user_token" \
+    --form-string title="Services Checker" \
+    --form-string message="$message_form" \
     https://api.pushover.net/1/messages.json
-    touch /tmp/IPChecker.lock
-  fi
+}
+#
+#
+#+------------------------------+
+#+---"Set & Import Variables"---+
+#+------------------------------+
+PATH=/sbin:/bin:/usr/bin:/home/jlivin25
+log=/home/pi/bin/script_logs/services_checker.log
+stamp=$(echo "`date +%H.%M`-`date +%d_%m_%Y`")
+#
+tested_ip=$(curl ipinfo.io/ip)
+#
+file_temp=/tmp/IPChecker.lock
+#
+#
+#+---------------------+
+#+---"Start Logging"---+
+#+---------------------+
+echo "#" >> $log
+echo "-----------------------------------------------------------------------------" >> $log
+echo "script STARTED - $stamp" >> $log
+#
+#
+#+------------------------+
+#+---"Import user info"---+
+#+------------------------+
+dir_name="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $dir_name >> $log
+source $dir_name/config.sh
+echo "I would source $dir_name/config.sh in real world" >> $log
+#
+#
+#+--------------------+
+#+---"Start Script"---+
+#+--------------------+
+if [ "$tested_ip" =! "$expected_ip" ]
+ then
+   echo "VPN is DOWN"
+ else
+   echo "VPN is UP"
+ #
+ #
+#  if test -f "$FILE"
+#  then echo "VPN Down but $FILE exists, a reset has not been completed"
+#     exit 0
+#  else
+#  echo "VPN Down, stopping transmission-daemon"
+#  systemctl stop transmission-daemon  
+#  fi
 fi
